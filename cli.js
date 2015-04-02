8c99c832d766fe7851e859e6db5c5142f0534aef
Max Ogden <max@maxogden.com>
2015-04-02 11:19:05 -0700

    update cli

--- cli.js
+++ cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-var lobby = require('./')()
+var lobby = require('./index.js')()
 var port = process.argv[2] || process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 5005
 var ip = process.env.OPENSHIFT_NODEJS_IP
 
